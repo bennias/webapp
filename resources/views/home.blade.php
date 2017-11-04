4b1af74a19cddf9438fc4bb4f651af20ecd0a973
bas <benedikt.asal@gmx.de>
2017-11-05 00:28:19 +0100

    started to add posts at home-view | changed design

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,58 @@
 @extends('layouts.app')
 
 @section('content')
+<section>
+    <div class="col-md-6 col-md-offset-3">
+        <header><h3>Allo Allo Waz up?</h3></header>
+        <form action="">
+            <div class="form-group">
+                <textarea class="form-control" name="new-post" id="new-post" rows="5" placeholder="Your Message"></textarea>
+                <button type="submit" class="btn btn-task">Create </button>
+            </div>
+        </form>
+    </div>
+</section>
+<section class="row posts">
+    <div class="col-md-6 col-md-3-offset">
+        <header><h3>What they say..</h3></header>
+        <article class="post">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias laudantium minus officia pariatur totam. Aspernatur doloremque eos expedita iusto magnam nesciunt optio possimus qui voluptatum? Consequatur debitis fugiat modi repellendus? </p>
+            <div class="info">
+                Posted by BAS on 04.11.2017
+            </div>
+            <div class="interaction">
+                <a href="#">Like</a>
+                <a href="#">Dislike</a>
+                <a href="#">Edit</a>
+                <a href="#">Delete</a>
+            </div>
+        </article>
+        <article class="post">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias laudantium minus officia pariatur totam. Aspernatur doloremque eos expedita iusto magnam nesciunt optio possimus qui voluptatum? Consequatur debitis fugiat modi repellendus? </p>
+            <div class="info">
+                Posted by BAS on 04.11.2017
+            </div>
+            <div class="interaction">
+                <a href="#">Like</a>
+                <a href="#">Dislike</a>
+                <a href="#">Edit</a>
+                <a href="#">Delete</a>
+            </div>
+        </article>
+
+    </div>
+</section>
+
+
+
+    {{--
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                {{--<div class="panel-heading">Profil</div>--}}
+                --}}
+{{--<div class="panel-heading">Profil</div>--}}{{--
+
 
                 <div class="panel-body">
                     @if (session('status'))
@@ -20,4 +67,5 @@
         </div>
     </div>
 </div>
+--}}
 @endsection
